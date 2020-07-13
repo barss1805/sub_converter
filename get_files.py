f9365699900e98a461a2e177cf5e2234ef38440d
barss1805 <barss1805@mail.ru>
2020-07-14 01:03:16 +0300

    variable

--- get_files.py
+++ get_files.py
@@ -1,8 +1,14 @@
-import glob, os
+# import glob, os
 
 
-os.chdir("D:\lessons\pyt_n\\11 Web Scraping with Python\\")
+# os.chdir("D:\lessons\pyt_n\\11 Web Scraping with Python\\")
 
-for file in glob.glob("*.srt"):
-    print(file)
-    print()
+# for file in glob.glob("*.srt"):
+#     print(file)
+#     print()
+
+
+# вызывает проводник виндовс без возможности выбора каталогов и папок
+# import webbrowser, os
+# path="C:/Users"
+# webbrowser.open(os.path.realpath(path))           
