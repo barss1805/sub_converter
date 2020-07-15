2d609201cb0132be55171e01d815acc049ebe5ff
barss1805 <barss1805@mail.ru>
2020-07-15 23:41:16 +0300

    rename file 'get_files.py ' to 'get_path_files.py'

--- get_path_files.py
+++ get_path_files.py
@@ -1,16 +1,12 @@
-import glob, os, easygui
+import easygui
+import os
 
 # call the window for selecting a folder with files
 # вызываем окно выбора папки с файлами
 input_file = easygui.diropenbox()
-# print(input_file)
-
-# os.chdir(input_file)
 
 
-# for file in glob.glob("*.srt"):
-#     print(file)
-#     print()
+# get the paths to all files in the folder
 # получаем пути ко всем файлам в папке
 for root, dirs, files in os.walk(input_file):
     for name in files:
