d02424bde7e7d5ed3c5fc1628190da1fc57e1420
barss1805 <barss1805@mail.ru>
2020-07-15 05:19:43 +0300

    Update get_files.py

--- get_files.py
+++ get_files.py
@@ -3,7 +3,7 @@ import glob, os, easygui
 # call the window for selecting a folder with files
 # вызываем окно выбора папки с файлами
 input_file = easygui.diropenbox()
-print(input_file)
+# print(input_file)
 
 # os.chdir(input_file)
 
@@ -11,8 +11,8 @@ print(input_file)
 # for file in glob.glob("*.srt"):
 #     print(file)
 #     print()
-for root, dirs, files in os.walk("D:\lessons\pyt_n\\11 Web Scraping with Python"):
-    print("ggggggggggggg")
-    for file in files:
-        if file.endswith(".srt"):
-             print(os.path.join(root, file))
+# получаем пути ко всем файлам в папке
+for root, dirs, files in os.walk(input_file):
+    for name in files:
+        # if name.endswith(".srt"):
+        print(os.path.join(root, name))
