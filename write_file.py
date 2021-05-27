2bc12785c607244640150408d8fdc59e6bf443b8
barss1805 <barss1805@mail.ru>
2021-05-27 16:45:43 +0300

    Delete write_file.py

--- write_file.py
+++
@@ -1,3 +0,0 @@
-def create_and_write_file(path_for_write_file, name_file, information_for_recording):
-    with open(path_for_write_file + name_file, "a", encoding='utf-8') as file:
-        file.write(information_for_recording)
