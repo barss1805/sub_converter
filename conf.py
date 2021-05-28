4ac8da5e20e875f63cff258c269fdbc9d6dbaaa2
Vitali Klein <barss1805@mail.ru>
2021-05-28 20:18:21 +0300

    add description to file conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,9 @@
+# variable to extention subtitles
 subtitle_extention = "srt"
+# variable to input language(used to change the old filename)
 input_lang = "en_US"
 
+# variable for the target language(used in generating a new filename)
 change_lang = "ru_RU"
-
+# variable for specifying the target language in the translator
 target_to_translate = "ru"
